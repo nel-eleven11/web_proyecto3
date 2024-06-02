dd48bc542965b553a8040ef36d79ae44ca94607f
Nelson <gar22434@uvg.edu.gt>
2024-06-02 15:50:18 -0600

    Feat: About done src pending

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,18 @@ import React from "react"
 
 const About = () => {
     return (
-        <div>
-        <h1>About</h1>
-        <p>This is the about page.</p>
-        </div>
+        <section id='about' className='container mh-100 card '>
+            <div class='row p-5'>
+                <div class='col-md-4 text-center'>
+                    <img src="" alt="" className='about-img' />
+                </div>
+                <div class='col-md-7 '>
+                    <h2 className='text-center'>About Me</h2>
+                    <p class="text-justify"></p>
+                    <p class="text-justify"></p>
+                </div>
+            </div>
+        </section>
     )
 }
 
