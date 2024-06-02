02049cd23e051ac0564220a46b84d89bbc81f9c0
Nelson <gar22434@uvg.edu.gt>
2024-06-01 22:43:58 -0600

    Add: Components folder and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    const styles = {
+        textAlign: 'center',
+        padding: '10px'
+    }
+    return (
+        <footer style={styles}>
+        <p>© 2024 Portafolio | Nelson García Bravatti | Universidad del Valle de Guatemala</p>
+        </footer>
+    )   
+}
+
+export default Footer
