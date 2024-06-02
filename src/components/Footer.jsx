a7d867be77db1b42e03618ccc9ec2a2c32574731
Nelson <gar22434@uvg.edu.gt>
2024-06-02 15:48:43 -0600

    Feat: Footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 
 const Footer = () => {
-    const styles = {
-        textAlign: 'center',
-        padding: '10px'
-    }
     return (
-        <footer style={styles}>
-        <p>© 2024 Portafolio | Nelson García Bravatti | Universidad del Valle de Guatemala</p>
-        </footer>
+        <div class='footer text-center'>
+            <span id='copyright'>© 2024 Portafolio | Nelson García Bravatti | Universidad del Valle de Guatemala | All rights reserved.</span>
+        </div>
     )   
 }
 
