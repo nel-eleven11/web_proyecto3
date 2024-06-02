7d76fd2dcd38b2a3987ef0de64ada6cd8953780e
Nelson <gar22434@uvg.edu.gt>
2024-06-02 15:55:32 -0600

    Fix: File name

---
+++ src/components/Proyects.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Proyects = () => {
+    return (
+        <section id='projects' class='projects container'>
+          <h2 class="text-center">My Projects</h2>
+          <div class='row'>
+    
+    
+          </div>
+        </section>
+      )
+}
+
+export default Proyects
