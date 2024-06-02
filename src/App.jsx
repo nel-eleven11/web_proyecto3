8225a9dae93cfc88f44272d1255c835ad1f4cde2
Nelson <gar22434@uvg.edu.gt>
2024-06-02 15:25:40 -0600

    Feat: App with all components

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer'
+import NavBar from './components/NavBar'
+import Proyects from './components/Proyects'
+import Contact from './components/Contact'
+import Banner from './components/Banner'
+import About from './components/About'
+
 
 function App() {
-  
 
   return (
     
     <div>
+      <NavBar />
+      <Banner />
+      <About />
+      <Proyects />
+      <Contact />
       <Footer />
     </div>
     
