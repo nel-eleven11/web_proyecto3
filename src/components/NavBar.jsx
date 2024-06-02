ad7d1cbc1104f952449d67aee4a277d218442cc1
Nelson <gar22434@uvg.edu.gt>
2024-06-02 15:49:53 -0600

    Feat: Navbar done img pending

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,10 +8,23 @@ const NavBar = () => {
         borderBottom: '1px solid #d1d1d1'
     }
     return (
-        <nav style={styles}>
-            
+        <nav class="navbar fixed-top shadow-sm navbar-expand-lg bg-light navbar-light p-3 py-4 py-lg-0 px-lg-5 px-4">
+            <a href="index.html" class="navbar-brand ml-lg-3">
+                <img src="" alt="website logo" width="60" height="60" />
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="navbar-nav m-auto py-0">
+                    <a href="#" class="nav-item nav-link active">Home</a>
+                    <a href="#about" class="nav-item nav-link">About Me</a>
+                    <a href="#projects" class="nav-item nav-link">Projects</a>
+                    <a href="#skills" class="nav-item nav-link">Skill</a>
+                    <a href="#contact" class="nav-item nav-link">Contact</a>
+                </div>
+            </div>
         </nav>
-        
     )   
 }
 
