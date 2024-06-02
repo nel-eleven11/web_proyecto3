be7531123ccd9e40fd3875d4ea74854cdcaed064
Nelson <gar22434@uvg.edu.gt>
2024-06-01 22:53:24 -0600

    Add: NavBar file

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+    const styles = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '10px',
+        borderBottom: '1px solid #d1d1d1'
+    }
+    return (
+        <nav style={styles}>
+            
+        </nav>
+        
+    )   
+}
+
+export default NavBar
