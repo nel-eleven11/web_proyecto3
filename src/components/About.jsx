e1440ba898ebcaeae913baecc12529d33827df3a
Nelson <gar22434@uvg.edu.gt>
2024-06-02 22:20:26 -0600

    Fix: More info in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,15 +10,21 @@ const About = () => {
                 <div class='col-md-7 '>
                     <h2 className='text-center'>About Me</h2>
                     <p class="text-justify">
-                    I am a 3rd year student studying Computer Science and Information Technology Engineering, 
-                    I am 20 years old and I am passionate about programming and software development. 
-                    Throughout my academic training, I have acquired solid knowledge in various programming 
-                    languages and technological tools. I am excited to work on challenging projects that allow me 
-                    to apply and expand my technical skills, and I am always looking for new opportunities to 
-                    learn and grow professionally. I am committed to excellence and innovation, and my goal is to 
-                    contribute meaningfully to the field of technology.
+                        I am a 20 years old and  3rd year student studying Computer Science and Information Technology Engineering in Guatemala. 
+                        Throughout my academic training, I have acquired solid knowledge in various programming 
+                        languages and technological tools. I am excited to work on challenging projects that allow me 
+                        to apply and expand my technical skills, and I am always looking for new opportunities to 
+                        learn and grow professionally. I am committed to excellence and innovation, and my goal is to 
+                        contribute meaningfully to the field of technology.
                     </p>
-                    <p class="text-justify"></p>
+
+                    <p class="text-justify">
+                        I like listening to music, playing video games, and watching movies. I am also a fan of fooball, specially for
+                        Real Madrid and Bayern Münich, I practice football and volleyball in my free time. Reading books is also one of 
+                        my things, recently I have been reading a series called Shadowhunters, which I highly recommend. In terms of music,
+                        my favorites genres are rock, alternativ rock and smooth jazz, some of my favorites bands are: Imagine Dragons, 
+                        Radiohead, Daft Punk, Acoustic Alchemy and Gorillaz. 
+                    </p>
                 </div>
             </div>
         </section>
