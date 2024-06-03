a16d24209d8d00fb4fbb48a522bdaeb64591baab
Nelson <gar22434@uvg.edu.gt>
2024-06-02 21:39:58 -0600

    Feat : About component done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,18 @@ const About = () => {
         <section id='about' className='container mh-100 card '>
             <div class='row p-5'>
                 <div class='col-md-4 text-center'>
-                    <img src="./img/niebla.jpg" alt="" className='about-img' />
+                    <img src="./img/nelson.jpg" alt="" className='about-img' />
                 </div>
                 <div class='col-md-7 '>
                     <h2 className='text-center'>About Me</h2>
                     <p class="text-justify">
-                        Hola soy Nelson Eduardo García Bravatti y soy estudiante de Ingeniería en Ciencias de la Computadción y tecnologías de la Información en la
-                        Universidad del Valle de Guatemala. 
+                    I am a 3rd year student studying Computer Science and Information Technology Engineering, 
+                    I am 20 years old and I am passionate about programming and software development. 
+                    Throughout my academic training, I have acquired solid knowledge in various programming 
+                    languages and technological tools. I am excited to work on challenging projects that allow me 
+                    to apply and expand my technical skills, and I am always looking for new opportunities to 
+                    learn and grow professionally. I am committed to excellence and innovation, and my goal is to 
+                    contribute meaningfully to the field of technology.
                     </p>
                     <p class="text-justify"></p>
                 </div>
