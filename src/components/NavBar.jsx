f0103125c93b6132e36d6845dd93b8671aaee4ec
Nelson <gar22434@uvg.edu.gt>
2024-06-02 21:39:41 -0600

    Fix: Imports corrected

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (
         <nav className="navbar fixed-top shadow-sm navbar-expand-lg bg-light navbar-light p-3 py-4 py-lg-0 px-lg-5 px-4">
             <a href="index.html" className="navbar-brand ml-lg-3">
-                <img src="./img/niebla.jpg" alt="website logo" width="60" height="60" />
+                <img src="./img/nelson-logo.png" alt="website logo" width="60" height="60" />
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
